7463596fdb2797bea19c4bb651d97ecc3973a304
sonicemerald <sonicemerald@gmail.com>
2014-11-05 16:11:31 -0800

    removed emblem and token prices

--- scraper.py
+++ scraper.py
@@ -125,6 +125,7 @@ def getTCGPlayerSetPrices(cardSet):
         dict = (("name", CardName[8:len(CardName)]), ("low", lowPrice[0:len(lowPrice)-6]), ("med", midPrice[0:len(midPrice)-6]), ("high", highPrice[0:len(highPrice)-6]))
         #dict = {"name": CardName[8:len(CardName)], "low": lowPrice[0:len(lowPrice)-6], "med": midPrice[0:len(midPrice)-6], "high": highPrice[0:len(highPrice)-6]}
         dict = OrderedDict(dict)
-        setArray.append(dict)
+        if((CardName.find("Emblem -") == -1) or (CardName.find("Token") == -1)):
+            setArray.append(dict)    
     return setArray
     # return "HI"
